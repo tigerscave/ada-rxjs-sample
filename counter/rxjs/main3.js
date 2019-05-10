5792ab7c33901de20215a2a9ee481ce28c893f27
cdra0624 <t.adaniya@adawarp.com>
2019-05-10 23:35:30 +0900

    feat(counter): add merge and map counter sample

--- counter/rxjs/main3.js
+++ counter/rxjs/main3.js
@@ -1,20 +1,31 @@
 'use strict';
 
+const { merge } = rxjs;
+const { map } = operators;
+
 const plusButton3 = document.getElementById('plusButton3');
 const minusButton3 = document.getElementById('minusButton3');
-
+const resetButton3 = document.getElementById('resetButton3');
 const countDisplay3 = document.getElementById('countDisplay3');
 
-const plusObservable =
-  fromEvent(plusButton3, 'click')
-  .pipe(scan(count => count + 1));
-  
-const minusObservable =
-  fromEvent(minusButton3, 'click');
+const plusObservable = fromEvent(plusButton3, 'click');
+const minusObservable = fromEvent(minusButton3, 'click');
+const resetObservable = fromEvent(resetButton3, 'click');
 
-const subscription =
-  plusObservable.subscribe(x => console.log('first: ' + x));
-const childSubscription =
-  minusObservable.subscribe(x => console.log('second: ' + x));
-
-subscription.add(childSubscription);
+merge(
+  plusObservable.pipe(map(() => 'plus')),
+  minusObservable.pipe(map(() => 'minus')),
+  resetObservable.pipe(map(() => 'reset'))
+)
+.pipe(
+  scan((a, e) => {
+    switch(e) {
+      case "plus": return a + 1;
+      case "minus": return a - 1;
+      case "reset": return 0;
+    }
+  }, 0)
+)
+.subscribe(val => {
+  countDisplay3.innerHTML = val;
+});
